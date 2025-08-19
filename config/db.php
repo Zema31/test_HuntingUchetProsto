e7d1971b2b5b0f67d8c8b96747df324ebe766f6a
Zema31 <zemfiraharisova@yandex.ru>
2025-08-20 01:13:57 +0300

    fix

--- config/db.php
+++ config/db.php
@@ -1,16 +1,10 @@
 <?php
 
 return [
-    // 'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=localhost;dbname=u3228172_default',
-    // 'username' => 'u3228172_default',
-    // 'password' => 'l8fDYy5zpVB7ay50',
-    // 'charset' => 'utf8',
-
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => 'l7xtcmb2',
+    'dsn' => 'mysql:host=localhost;dbname=u3228172_default',
+    'username' => 'u3228172_default',
+    'password' => 'l8fDYy5zpVB7ay50',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
