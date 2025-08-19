574b7c8b2b72a66d41e08163c3e7707183b132db
Zema31 <zemfiraharisova@yandex.ru>
2025-08-20 01:13:23 +0300

    fix

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=u3228172_default',
+    // 'username' => 'u3228172_default',
+    // 'password' => 'l8fDYy5zpVB7ay50',
+    // 'charset' => 'utf8',
+
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'l7xtcmb2',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
